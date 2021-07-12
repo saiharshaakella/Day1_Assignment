aacbb349a78366f25c9d642f8e0f984e9b62bea0
Sai Bhanu Harsha Vardhan Akella <66198044+saiharshaakella@users.noreply.github.com>
2021-07-12 22:12:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Button from "./Button"
 const Apple=()=>{
     return(
         <div>
-            <h1 style={{textAlign : "center"}}>Hello World!!</h1>
+            <h1 style={{textAlign : "center"}}>Hello LetsUpgrade!!</h1>
             <h2>I'm Sai Bhanu Harsha Vardhan Akella from Andhra Pradesh. Currently I'm pursing my B.Tech 3rd Year in Kakinada Institute of Engineering and Technology,Kakinada</h2>
             <Button buttonText="Education"/>
             <Button buttonText="Skills"/>
