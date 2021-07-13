ec1c61ce516a29da7969d5dd24b2a4996f301c33
Sai Bhanu Harsha Vardhan Akella <66198044+saiharshaakella@users.noreply.github.com>
2021-07-13 14:05:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const Apple=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign : "center"}}>Hello LetsUpgrade!!</h1>
+            <h2>I'm Sai Bhanu Harsha Vardhan Akella from Andhra Pradesh. Currently I'm pursing my B.Tech 3rd Year in Kakinada Institute of Engineering and Technology,Kakinada</h2>
+            <Button buttonText="Education"/>
+            <Button buttonText="Skills"/>
+            <Button buttonText="Achievements"/>
+        </div>
+        
+    )
+}
+
+export default Apple
